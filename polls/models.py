be4f94c7b17ca88c1baebcf5a1a5a5e4f24a233d
sajalsinghal7 <sajalsinghal7@gmail.com>
2017-07-30 09:46:56 +0530

    Updating polls after data loss

--- polls/models.py
+++ polls/models.py
@@ -1,19 +1,11 @@
 from django.db import models
-import datetime 
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
     	return self.question_text
-    # def was_published_recently(self):
-    # 	if self.pub_date.date() < (datetime.datetime.now().date() - datetime.timedelta(days=1)):
-    # 		return False;
-    # 	else:
-    # 		return True;
-    def was_published_recently(self):
-    	now = timezone.now()
-    	return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
